6e171e12a2a1e2491b18ade5c793d2167561eb3a
thanhdatpd <thanhdatppro@gmail.com>
2018-11-27 02:41:12 +0700

    add twrp config/ just test

--- full_bicot.mk
+++ full_bicot.mk
@@ -14,6 +14,10 @@
 # limitations under the License.
 #
 
+#for twrp
+PRODUCT_RELEASE_NAME := bicot
+$(call inherit-product, build/target/product/embedded.mk)
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
