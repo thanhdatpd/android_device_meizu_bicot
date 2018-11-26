35490a57425f9ddb210e7c3de653b6f8bdd61529
thanhdatpd <thanhdatppro@gmail.com>
2018-11-26 23:14:28 +0700

    Update configSDK.mk

--- config/configSDK.mk
+++ config/configSDK.mk
@@ -1,2 +1,5 @@
 LOCAL_PATH := $(call my-dir)
-LOCAL_SDK_VERSION := current
+LOCAL_SDK_VERSION := 28
+#other add when build to fix some error without know =((( by thanhdatpd
+ALLOW_MISSING_DEPENDENCIES := true
+whitelisted_modules := framework-res__auto_generated_rro SystemUI__auto_generated_rro Bluetooth__auto_generated_rro Settings__auto_generated_rro
