2a9a407d4b41b98caf5e178327868bdd3036bb12
thanhdatpd <thanhdatppro@gmail.com>
2018-12-22 08:45:26 +0700

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -49,6 +49,7 @@ TARGET_BOOTLOADER_BOARD_NAME := MSM8953
 TARGET_NO_BOOTLOADER := true
 
 # Kernel
+TARGET_NO_KERNEL := true
 #BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78af000 androidboot.selinux=permissive buildvariant=eng
 #BOARD_KERNEL_BASE := 0x80000000
 #BOARD_KERNEL_PAGESIZE := 4096
