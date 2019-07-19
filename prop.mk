032d796b004a0befb275650beb4808cd126845fa
thanhdatpd <thanhdatppro@gmail.com>
2019-07-19 22:45:00 +0700

    Update prop.mk

--- prop.mk
+++ prop.mk
@@ -9,26 +9,8 @@ audio.deep_buffer.media=true \
 audio.offload.disable=true \
 audio.offload.min.duration.secs=30 \
 audio.offload.video=true \
-#persist.vendor.audio.fluence.speaker=true \
-#persist.vendor.audio.fluence.voicecall=true \
-#persist.vendor.audio.fluence.voicerec=true \
-#persist.vendor.bt.enable.splita2dp=false \
 ro.vendor.audio.sdk.fluencetype=fluence \
 ro.vendor.audio.sdk.ssr=false \
-#vendor.audio.flac.sw.decoder.24bit=true \
-#vendor.audio.offload.buffer.size.kb=64 \
-#vendor.audio.offload.gapless.enabled=true \
-#vendor.audio.offload.multiaac.enable=true \
-#vendor.audio.offload.multiple.enabled=false \
-#vendor.audio.offload.track.enable=true \
-#vendor.audio.parser.ip.buffer.size=0 \
-#vendor.audio.playback.mch.downsample=true \
-#vendor.audio.pp.asphere.enabled=false \
-#vendor.audio.safx.pbe.enabled=true \
-#vendor.audio.tunnel.encode=false \
-#vendor.audio.use.sw.alac.decoder=true \
-#vendor.audio.use.sw.ape.decoder=true \
-#vendor.audio_hal.period_size=192 \
 vendor.voice.conc.fallbackpath=deep-buffer \
 vendor.voice.path.for.pcm.voip=true \
 vendor.voice.playback.conc.disabled=true \
@@ -52,7 +34,6 @@ media.camera.ts.monotonic=1 \
 persist.camera.gyro.disable=0 \
 persist.camera.isp.clock.optmz=0 \
 persist.camera.stats.test=5 \
-#persist.vendor.qti.telephony.vt_cam_interface=1 \
 vidc.enc.dcvs.extra-buff-count=2
 
 # Cne/Dpm
@@ -104,82 +85,76 @@ ro.gps.agps_provider=1
 
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
-av.debug.disable.pers.cache=1 \
-media.aac_51_output_enabled=true \
-media.msm8956hw=0 \
-media.stagefright.audio.sink=280 \
-mm.enable.qcom_parser=1048575 \
-mm.enable.smoothstreaming=true \
-mmp.enable.3g2=true \
-#vendor.audio.hw.aac.encoder=true \
-#vendor.vidc.dec.downscalar_height=1088 \
-#vendor.vidc.dec.downscalar_width=1920 \
-#vendor.vidc.disable.split.mode=1 \
-#vendor.vidc.enc.disable.pq=true \
-#vendor.vidc.enc.disable_bframes=1
+#av.debug.disable.pers.cache=1 \
+#media.aac_51_output_enabled=true \
+#media.msm8956hw=0 \
+#media.stagefright.audio.sink=280 \
+#mm.enable.qcom_parser=1048575 \
+#mm.enable.smoothstreaming=true \
+##mmp.enable.3g2=true \
 
 # Perf
-PRODUCT_PROPERTY_OVERRIDES += \
-ro.sys.fw.dex2oat_thread_count=4 \
-ro.vendor.extension_library=libqti-perfd-client.so
+#PRODUCT_PROPERTY_OVERRIDES += \
+#ro.sys.fw.dex2oat_thread_count=4 \
+#ro.vendor.extension_library=libqti-perfd-client.so
 
 # Netmgrd
-PRODUCT_PROPERTY_OVERRIDES += \
-ro.use_data_netmgrd=true \
-persist.data.netmgrd.qos.enable=true \
-persist.data.mode=concurrent
+#PRODUCT_PROPERTY_OVERRIDES += \
+#ro.use_data_netmgrd=true \
+##persist.data.netmgrd.qos.enable=true \
+#persist.data.mode=concurrent
 
 # Nitz
-PRODUCT_PROPERTY_OVERRIDES += \
-persist.rild.nitz_plmn="" \
-persist.rild.nitz_long_ons_0="" \
-persist.rild.nitz_long_ons_1="" \
-persist.rild.nitz_long_ons_2="" \
-persist.rild.nitz_long_ons_3="" \
-persist.rild.nitz_short_ons_0="" \
-persist.rild.nitz_short_ons_1="" \
-persist.rild.nitz_short_ons_2="" \
-persist.rild.nitz_short_ons_3=""
+#PRODUCT_PROPERTY_OVERRIDES += \
+#persist.rild.nitz_plmn="" \
+#persist.rild.nitz_long_ons_0="" \
+#persist.rild.nitz_long_ons_1="" \
+#persist.rild.nitz_long_ons_2="" \
+#persist.rild.nitz_long_ons_3="" \
+#persist.rild.nitz_short_ons_0="" \
+#persist.rild.nitz_short_ons_1="" \
+#persist.rild.nitz_short_ons_2="" \
+#persist.rild.nitz_short_ons_3=""
 
 # Radio
-PRODUCT_PROPERTY_OVERRIDES += \
-DEVICE_PROVISIONED=1 \
-persist.data.iwlan.enable=true \
-persist.dbg.ims_volte_enable=1 \
-persist.dbg.volte_avail_ovr=1 \
-persist.dbg.vt_avail_ovr=1 \
-persist.dbg.wfc_avail_ovr=0 \
-persist.radio.apm_sim_not_pwdn=1 \
-persist.radio.calls.on.ims=0 \
-persist.radio.csvt.enabled=false \
-persist.radio.hw_mbn_update=0 \
-persist.radio.jbims=0 \
-persist.radio.mt_sms_ack=20 \
-persist.radio.multisim.config=dsds \
-persist.radio.sw_mbn_update=0 \
-persist.radio.videopause.mode=1 \
-persist.vendor.radio.custom_ecc=1 \
-persist.vendor.radio.rat_on=combine \
-#persist.vendor.radio.sib16_support=1 \
-ril.subscription.types=NV,RUIM \
-rild.libargs=-d/dev/smd0 \
-rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
-ro.telephony.call_ring.multiple=false \
-ro.telephony.default_network=22,20 \
-service.qti.ims.enabled=1 \
-telephony.lteOnCdmaDevice=1
+#PRODUCT_PROPERTY_OVERRIDES += \
+#DEVICE_PROVISIONED=1 \
+#persist.data.iwlan.enable=true \
+#persist.dbg.ims_volte_enable=1 \
+#persist.dbg.volte_avail_ovr=1 \
+#persist.dbg.vt_avail_ovr=1 \
+#persist.dbg.wfc_avail_ovr=0 \
+#persist.radio.apm_sim_not_pwdn=1 \
+#persist.radio.calls.on.ims=0 \
+#persist.radio.csvt.enabled=false \
+#persist.radio.hw_mbn_update=0 \
+#persist.radio.jbims=0 \
+#persist.radio.mt_sms_ack=20 \
+#persist.radio.multisim.config=dsds \
+#persist.radio.sw_mbn_update=0 \
+#persist.radio.videopause.mode=1 \
+#persist.vendor.radio.custom_ecc=1 \
+#persist.vendor.radio.rat_on=combine \
+
+#ril.subscription.types=NV,RUIM \
+#rild.libargs=-d/dev/smd0 \
+#rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
+#ro.telephony.call_ring.multiple=false \
+#ro.telephony.default_network=22,20 \
+#service.qti.ims.enabled=1 \
+#telephony.lteOnCdmaDevice=1
 
 # Time Services
-PRODUCT_PROPERTY_OVERRIDES += \
-persist.timed.enable=true
+#PRODUCT_PROPERTY_OVERRIDES += \
+#persist.timed.enable=true
 
 # Tcp
-PRODUCT_PROPERTY_OVERRIDES += \
-net.tcp.2g_init_rwnd=10
+#PRODUCT_PROPERTY_OVERRIDES += \
+#net.tcp.2g_init_rwnd=10
 
 # Usb
-PRODUCT_PROPERTY_OVERRIDES += \
-persist.sys.usb.config.extra=none
+#PRODUCT_PROPERTY_OVERRIDES += \
+#persist.sys.usb.config.extra=none
 
 # Wifi
 PRODUCT_PROPERTY_OVERRIDES += \
