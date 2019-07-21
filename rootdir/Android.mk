04a0eab1e9a11ae32e26de006c94b74c2faf46dd
thanhdatpd <thanhdatppro@gmail.com>
2019-07-21 21:20:10 +0700

    Update Android.mk

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -66,6 +66,16 @@ LOCAL_SRC_FILES    := init.cm.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.rc
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := init.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+
+
 
 
 
