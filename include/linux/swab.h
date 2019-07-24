0529b48dfbc5a3d4386e59a9079b2274f85814a9
thanhdatpd <thanhdatppro@gmail.com>
2019-07-25 02:39:09 +0700

    Update swab.h

--- include/linux/swab.h
+++ include/linux/swab.h
@@ -1,6 +1,6 @@
 #ifndef _LINUX_SWAB_H
 #define _LINUX_SWAB_H
-#include <uapi/linux/swab.h>
+//#include <uapi/linux/swab.h>
 # define swab16 __swab16
 # define swab32 __swab32
 # define swab64 __swab64
