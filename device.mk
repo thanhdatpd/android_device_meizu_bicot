643849db141bc14ad4f64ded04ffaaedea7642d6
thanhdatpd <thanhdatppro@gmail.com>
2019-07-28 15:30:28 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -75,10 +75,6 @@ PRODUCT_PACKAGES_DEBUG += \
 
 
 PRODUCT_PACKAGES += \
-    copybit.msm8953 \
-    gralloc.msm8953 \
-    hwcomposer.msm8953 \
-    memtrack.msm8953 \
     liboverlay \
     libjni_livedisplay \
     libtinyxml
