0c5c6d179b7402d49a40725d5a6136f729905563
thanhdatpd <thanhdatppro@gmail.com>
2019-08-02 00:32:20 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -373,7 +373,7 @@ PRODUCT_COPY_FILES += \
 
 # Wifi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi \
     libcld80211 \
     libqsap_sdk \
     libQWiFiSoftApCfg \
