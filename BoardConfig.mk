1ac0d4081fa0c3e0711c0443242dc9ea6d7a7f7d
thanhdatpd <thanhdatppro@gmail.com>
2019-08-06 04:36:19 +0700

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -76,7 +76,9 @@ BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_RAMDISK_OFFSET     := 0x01000000
-TARGET_PREBUILT_KERNEL := device/meizu/bicot/prebuilt/boot.img-kernel
+#TARGET_PREBUILT_KERNEL := device/meizu/bicot/prebuilt/boot.img-kernel
+#try using from yuan9
+TARGET_PREBUILT_KERNEL := device/meizu/bicot/prebuilt/zImage
 BOARD_KERNEL_IMAGE_NAME := boot.img-kernel
 KERNEL_MAKE_FLAGS += CFLAGS_MODULE="-fno-pic"
 
